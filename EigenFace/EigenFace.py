ffe0955fdff668b74fc348bb674b816ec2f4755b
Dan <dmgaldi@gmail.com>
2013-12-30 19:04:38 +0000

    added comments, basic error handling, and basic computations

--- EigenFace/EigenFace.py
+++ EigenFace/EigenFace.py
@@ -4,15 +4,41 @@ from PIL import Image
 class EigenFace(object):
 
     def __init__(self):
-        pass
+        self.eigenfaces = []
 
     def TrainWithImages(self, imageNames, res=None):
+        """
+        Trains the EigenFace model with a set of images with the same resolution
+
+        Parameters: imageNames: a python list of image file locations
+
+                    res: a tuple consisting of the length and width of the images in pixels
+                    
+        Returns: A numpy array of EigenFaces
+        """
+        
         assert(res is not None)
-        trainingMatrix = np.zeros(shape=(res[0] * res[1], len(imageNames)))
+
+        N = len(imageNames) #number of Training Images
+        X = res[0]
+        Y = res[1]
+
+        trainingMatrix = np.zeros(shape=(X * Y, N))
         
         for i, imageName in enumerate(imageNames):
-            img = Image.open(imageName).convert('L')        
-            trainingMatrix[:,i] = np.array(img)
+            img = Image.open(imageName).convert('L')
+
+            if not (img.size[0] == res[0] and img.size[1] == res[1]):
+                raise ValueError("Invalid dimensions for image " + str(i))
+   
+            trainingMatrix[:,i] = np.ndarray.flatten(np.array(img))
+
+            mean = np.mean(trainingMatrix, axis=0)
+
+            print trainingMatrix
+            print mean
+        
+        
     
 ef = EigenFace()
-ef.TrainWithImage(["../Images/zuck.jpg"])
+ef.TrainWithImages(["../Images/zuck.jpg"], (634, 396))
