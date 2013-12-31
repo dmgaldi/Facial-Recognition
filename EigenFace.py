bd7d372af6d46edda66ba3bc579d267138058cf8
Dan <dmgaldi@gmail.com>
2013-12-31 18:46:07 +0000

    Finished PCA computation

--- EigenFace.py
+++ EigenFace.py
@@ -6,6 +6,8 @@ class EigenFace(object):
 
     def __init__(self):
         self.eigenfaces = []
+        self.imageNames = []
+        self.res = None
 
     def TrainWithImages(self, imageNames, res=None):
         """
@@ -21,8 +23,8 @@ class EigenFace(object):
         assert(res is not None)
 
         N = len(imageNames) #number of Training Images
-        X = res[0] #dim1 of image
-        Y = res[1] #dim2 of image
+        X = res[0] #rows of pixels of image
+        Y = res[1] #cols of pixels of image
 
         trainingMatrix = np.zeros(shape=(X * Y, N))
         
@@ -34,10 +36,24 @@ class EigenFace(object):
    
             trainingMatrix[:,i] = np.ndarray.flatten(np.array(img))
 
-        ## Compute Covariance
-        S = Utility.Covariance(trainingMatrix)
-        print S
+        ## Perform PCA (limited by colums of trainingMatrix)
+        eigvals, eigvectors = Utility.PCA(trainingMatrix, Y)
 
-    
+        self.res = res
+        self.imageNames = imageNames
+        self.eigenfaces = eigvectors.copy()
+
+    def TrainSingleImage(self, imageName):
+        img = Image.open(imageName.convert('L'))
+        if self.res is None:
+            self.res = (img.size[0], img.size[1])
+            self.imageNames.append(imageName)
+        else:
+            if not (img.size[0] == res[0] and img.size[1] == res[1]):
+                raise ValueError("Invalid dimensions for new image")
+            else:
+                self.imageNames.append(imageName)
+                self.TrainWithImages(imageNames, res)
+
 ef = EigenFace()
 ef.TrainWithImages(["Images/zuck.jpg"], (634, 396))
