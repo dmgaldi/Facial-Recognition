5dec88a95219f7c9a74f5eebb9e9bbec66196aee
Dan <dmgaldi@gmail.com>
2014-01-03 03:16:29 +0000

    updated Utility.py

--- Utility.py
+++ Utility.py
@@ -1,28 +1,10 @@
+&
 import numpy as np
 
-def Covariance(m):
+
+def PCA(m, n):
     """
-    Computes the covariance matrix given a matrix
-
-    Parameters: m: a numpy array
-    
-
-    Returns: A numpy covariance array
-    """
-    mean = np.mean(m, axis=1)
-
-    for i in range(0, m.shape[1]):
-            if i == 0:
-                S = (m[:,i] - mean) * np.transpose((m[:,i] - mean)) ## Covariance Matrix
-            else:
-                S += (m[:,i] - mean) * np.transpose((m[:,i] - mean))
-
-    S /= m.shape[1]
-    return S
-
-def PCD(m, n):
-    """
-    Computes the principal component decomposition of the input matrix
+    Computes the principal component analysis of the input matrix
 
     Parameters: m: numpy array to be decomposed
 
@@ -31,8 +13,14 @@ def PCD(m, n):
     Returns: A numpy array of Eigenvectors and a python list of Eigenvalues
     """
     mean = np.mean(m, axis=1)
-    if n > m.shape[1]:
-        pass
-    else:
+    m = m - mean
+    if m.shape[1] <= m.shape[0]:
         C = np.dot(np.transpose(m), m)
-        eig = np.linalg.eigh(C)
+        eigvals, eigvectors = np.linalg.eigh(C)
+        eigvectors = np.dot(m, eigvectors)
+        for i in range(0, m.shape[1]):
+            eigvectors[:,i] = eigvectors[:,i]/np.linalg.norm(eigvectors[:,i])
+    else:
+        C = np.dot(m, np.transpose(m))
+        eigvals, eigvectors = np.linalg.eigh(C)
+    return eigvals, eigvectors
